b64281c2a7efc424d5088c9af75a8bbe233bfdd0
egor594bed <egor594work@gmail.com>
2023-08-27 16:20:25 +0300

    add default employee list

--- client/src/components/EmployeeListPage/EmployeeItem.tsx
+++ client/src/components/EmployeeListPage/EmployeeItem.tsx
@@ -1,10 +1,25 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { EmployeeDtoWhithPosition } from "../../types/EmployeeTypes";
+import { Accordion, AccordionSummary, Typography } from "@mui/material";
+import { positionsRu } from "../../const/positionLocalization";
 
 type Props = {
   employee: EmployeeDtoWhithPosition;
 };
 
 export const EmployeeItem: FC<Props> = ({ employee }) => {
-  return <div>EmployeeItem</div>;
+  return (
+    <Accordion expanded={false}>
+      <AccordionSummary style={{ cursor: "default" }}>
+        <Typography sx={{ width: "50%", flexShrink: 0 }}>
+          {employee.fullname}
+        </Typography>
+        <Typography
+          sx={{ color: "text.secondary", textAlign: "end", width: "50%" }}
+        >
+          {positionsRu[employee.position.positionId]}
+        </Typography>
+      </AccordionSummary>
+    </Accordion>
+  );
 };
