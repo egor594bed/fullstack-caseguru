42ed72029c2e92f70ce71e69bd1c1841925e6082
egor594bed <egor594work@gmail.com>
2023-08-28 03:37:45 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({ credentials: true, origin: "http://localhost:3000" });
+  app.enableCors({ credentials: true, origin: true });
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
